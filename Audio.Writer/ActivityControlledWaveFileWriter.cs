f6b931fdd848f60933b8685818fd50f51cde889c
Kana <orzfirst@vip.qq.com>
2021-05-15 00:10:16 -0700

    add activity controlled wave file writers

---
+++ Audio.Writer/ActivityControlledWaveFileWriter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Microsoft.Psi;
+using Microsoft.Psi.Audio;
+
+namespace OpenSense.Component.Psi.Audio {
+    /// <summary>
+    /// This component will write wave file only when the activity indicator is on.
+    /// </summary>
+    public class ActivityControlledWaveFileWriter : IConsumer<(AudioBuffer, bool)>, IDisposable {
+
+        private readonly string _filename;
+
+        private WaveDataWriterClass writer;
+
+        public Receiver<(AudioBuffer, bool)> In { get; }
+
+        public ActivityControlledWaveFileWriter(Pipeline pipeline, string filename) {
+            In = pipeline.CreateReceiver<(AudioBuffer, bool)>(this, Process, nameof(In));
+            _filename = filename ?? throw new ArgumentNullException(nameof(filename));
+        }
+
+        private void Process((AudioBuffer, bool) data, Envelope envelope) {
+            var (frame, activity) = data;
+            if (!activity) {
+                return;
+            }
+            if (writer == null) {
+                var filestream = new FileStream(_filename, FileMode.Create);
+                var format = frame.Format.DeepClone();
+                writer = new WaveDataWriterClass(filestream, format);
+            }
+            writer.Write(frame.Data);
+        }
+
+        public void Dispose() {
+            if (writer != null) {
+                writer.Dispose();
+                writer = null;
+            }
+        }
+    }
+}
---
+++ Audio.Writer/ActivityControlledWaveFilesWriter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using Microsoft.Psi;
+using Microsoft.Psi.Audio;
+
+namespace OpenSense.Component.Psi.Audio {
+    /// <summary>
+    /// This component will write multiple wave files when the activity indicator is on.
+    /// </summary>
+    public class ActivityControlledWaveFilesWriter : IConsumer<(AudioBuffer, bool)>, IDisposable {
+
+        private readonly Func<Envelope, string> _filenameGenerator;
+
+        private WaveDataWriterClass writer;
+
+        public Receiver<(AudioBuffer, bool)> In { get; }
+
+        protected ActivityControlledWaveFilesWriter(Pipeline pipeline) {
+            In = pipeline.CreateReceiver<(AudioBuffer, bool)>(this, Process, nameof(In));
+        }
+
+        public ActivityControlledWaveFilesWriter(Pipeline pipeline, Func<Envelope, string> filenameGenerator) : this(pipeline) {
+            _filenameGenerator = filenameGenerator ?? throw new ArgumentNullException(nameof(filenameGenerator));
+        }
+
+        private void DisposeWriter() {
+            if (writer != null) {
+                writer.Dispose();
+                writer = null; 
+            }
+        }
+
+        private void Process((AudioBuffer, bool) data, Envelope envelope) {
+            var (frame, activity) = data;
+            if (activity) {
+                if (writer == null) {
+                    var filename = _filenameGenerator(envelope);
+                    var filestream = new FileStream(filename, FileMode.Create);
+                    var format = frame.Format.DeepClone();
+                    writer = new WaveDataWriterClass(filestream, format);
+                }
+                writer.Write(frame.Data);
+            } else {
+                DisposeWriter();
+            }
+        }
+
+        public void Dispose() {
+            DisposeWriter();
+        }
+    }
+}
