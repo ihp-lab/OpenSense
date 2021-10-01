1f6b4c4f6172e5b0443a78263e4c9e74baeec69d
Kana <orzfirst@vip.qq.com>
2021-09-30 18:14:45 -0700

    ColorVideoVisualizer Refactor

--- Imaging.Visualizer.Wpf/ColorVideoVisualizer.cs
+++ Imaging.Visualizer.Wpf/ColorVideoVisualizer.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Media.Imaging;
 using Microsoft.Psi;
 using Microsoft.Psi.Imaging;
-using OpenSense.Component.Imaging.Visualizer.Common;
 
 namespace OpenSense.Component.Imaging.Visualizer {
     public class ColorVideoVisualizer : IConsumer<Shared<Image>>, INotifyPropertyChanged {
@@ -20,40 +20,119 @@ namespace OpenSense.Component.Imaging.Visualizer {
         }
         #endregion
 
+        #region Settings
+
+        #endregion
+
+        #region Ports
         public Receiver<Shared<Image>> In { get; private set; }
+        #endregion
+
+        #region Binding Fields
+        private WriteableBitmap image = null;
+
+        public WriteableBitmap Image {
+            get => image;
+            private set => SetProperty(ref image, value);
+        }
+
+        private int frameRate = -1;
+
+        public int FrameRate {
+            get => frameRate;
+            private set => SetProperty(ref frameRate, value);
+        } 
+        #endregion
 
         public ColorVideoVisualizer(Pipeline pipeline) {
             In = pipeline.CreateReceiver<Shared<Image>>(this, Process, nameof(In));
-            pipeline.PipelineCompleted += PipelineCompleted;
-
-            display.PropertyChanged += (sender, e) => {
-                if (e.PropertyName == nameof(display.VideoImage)) {
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Image)));
-                }
-            };
-            display.ReceivedFrames.PropertyChanged += (sender, e) => {
-                if (e.PropertyName == nameof(display.RenderedFrames.Rate)) {
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FrameRate)));
-                }
-            };
         }
 
         private void Process(Shared<Image> frame, Envelope envelope) {
-            display.Update(frame);
+
+           lock(imageSwapLock) {
+                imageToRender?.Dispose();
+                imageToRender = frame.AddRef();
+            }
+
+            UpdateFrame(envelope.OriginatingTime);
         }
 
-        private void PipelineCompleted(object sender, PipelineCompletedEventArgs e) {
-            display.Clear();
+        #region Bitmap Helpers
+        private object imageSwapLock = new object();
+
+        private Shared<Image> imageToRender = null;
+
+        /// <summary>
+        /// Register this call back on UI thread's dispatcher
+        /// </summary>
+        public void RenderingCallback(object sender, EventArgs args) {//Modification of canvas should be on UI thread, otherwise will get an exception
+            lock (imageSwapLock) {
+                if (imageToRender is null) {
+                    return;
+                }
+                try {
+                    if (Image is null) {
+                        Image = CreateEmptyBitmap(imageToRender.Resource);
+                    }
+                    WriteToBitmap(imageToRender.Resource, Image);
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Image)));
+                } finally {
+                    imageToRender.Dispose();
+                    imageToRender = null;
+                }
+            }
         }
 
-        private DisplayVideo display = new DisplayVideo();
+        private static System.Windows.Media.PixelFormat ConvertPixelFormat(PixelFormat pixelFormat) => pixelFormat switch {
+            PixelFormat.Gray_8bpp => System.Windows.Media.PixelFormats.Gray8,
+            PixelFormat.Gray_16bpp => System.Windows.Media.PixelFormats.Gray16,
+            PixelFormat.BGR_24bpp => System.Windows.Media.PixelFormats.Bgr24,
+            PixelFormat.BGRX_32bpp => System.Windows.Media.PixelFormats.Bgr32,
+            PixelFormat.BGRA_32bpp => System.Windows.Media.PixelFormats.Bgra32,
+            _ => throw new InvalidOperationException("Unsupported /psi pixel format"),
+        };
 
-        public WriteableBitmap Image {
-            get => display.VideoImage;
+        private static WriteableBitmap CreateEmptyBitmap(Image image) {
+            var pixelFormat = ConvertPixelFormat(image.PixelFormat);
+            var width = image.Width;
+            var height = image.Height;
+            var result = new WriteableBitmap(width, height, dpiX: 96, dpiY: 96, pixelFormat, palette: null);
+            return result;
         }
 
-        public int FrameRate {
-            get => display.ReceivedFrames.Rate;
+        private static void WriteToBitmap(Image source, WriteableBitmap destination) {
+            if (destination.PixelWidth != source.Width || destination.PixelHeight != source.Height || destination.Format != ConvertPixelFormat(source.PixelFormat)) {
+                throw new InvalidOperationException("Frame size/format in-consistant");
+            }
+            var width = source.Width;
+            var height = source.Height;
+            var rect = new System.Windows.Int32Rect(x: 0, y: 0, width: width, height: height);
+            var bufferPtr = source.ImageData;
+            var bufferSize = source.Size;
+            var stride = source.Stride;
+            destination.WritePixels(rect, bufferPtr, bufferSize, stride);//Lock() and Unlock() not needed for WritePixels()
         }
+        #endregion
+
+        #region FrameRate Helpers
+        private const int frameBufferSize = 60;
+        private DateTime[] frames = new DateTime[frameBufferSize];
+        private long count = 0;
+
+        private void UpdateFrame(DateTime time) {
+            if (count < frameBufferSize) {
+                count++;
+                return;
+            }
+            var index = count % frameBufferSize;
+            var otherTime = frames[index];
+            frames[index] = time;
+            count++;
+            var diff = time - otherTime;
+            var rate = frameBufferSize / diff.TotalSeconds;
+            FrameRate = (int)rate;
+        }
+        #endregion
     }
 }
--- Imaging.Visualizer.Wpf/ColorVideoVisualizerInstanceControl.xaml.cs
+++ Imaging.Visualizer.Wpf/ColorVideoVisualizerInstanceControl.xaml.cs
@@ -1,9 +1,30 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using OpenSense.Component.Imaging.Visualizer;
 
 namespace OpenSense.Wpf.Component.Imaging.Visualizer {
     public partial class ColorVideoVisualizerInstanceControl : UserControl {
+
+        private ColorVideoVisualizer ViewModel => (ColorVideoVisualizer)DataContext;
+
         public ColorVideoVisualizerInstanceControl() {
             InitializeComponent();
+        }
+
+        private void UserControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
+            if (e.OldValue is ColorVideoVisualizer old) {
+                CompositionTarget.Rendering -= old.RenderingCallback;
+            }
+            if (e.NewValue is ColorVideoVisualizer @new) {
+                CompositionTarget.Rendering += @new.RenderingCallback;//will be called before every time WPF wants to render a frame
+            }
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e) {
+            if (ViewModel != null) {
+                CompositionTarget.Rendering -= ViewModel.RenderingCallback;
+            }
         }
     }
 }
