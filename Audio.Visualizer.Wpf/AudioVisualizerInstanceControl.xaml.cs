41d54e0c1ddac68a342ef968e4ef9b42bf9c3d84
Kana <orzfirst@vip.qq.com>
2022-04-14 19:56:37 -0700

    Make visualizer canvas size adaptive to render area size

--- Audio.Visualizer.Wpf/AudioVisualizerInstanceControl.xaml.cs
+++ Audio.Visualizer.Wpf/AudioVisualizerInstanceControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using OpenSense.Component.Audio.Visualizer;
 
@@ -11,19 +13,40 @@ namespace OpenSense.Wpf.Component.Audio.Visualizer {
             InitializeComponent();
         }
 
-        private void UserControl_DataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e) {
+        private void UserControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
             if (e.OldValue is AudioVisualizer old) {
                 CompositionTarget.Rendering -= old.RenderingCallback;
             }
             if (e.NewValue is AudioVisualizer @new) {
                 CompositionTarget.Rendering += @new.RenderingCallback;
             }
+            UpdateImageSize();
         }
 
-        private void UserControl_Unloaded(object sender, System.Windows.RoutedEventArgs e) {
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e) {
             if (ViewModel != null) {
                 CompositionTarget.Rendering -= ViewModel.RenderingCallback;
             }
+        }
+
+        private void ImageCanvas_Changed(object sender, RoutedEventArgs e) {
+            UpdateImageSize();
+            e.Handled = true;
+        }
+
+        private void UpdateImageSize() {
+            if (ImageCanvas.ActualWidth == 0 || ImageCanvas.ActualHeight == 0) {
+                //To bypass false invokes, this kind of invokation happens every time after the image width and height are set.
+                return;
+            }
+            if (ViewModel is null) {
+                return;
+            }
+            var dpi = VisualTreeHelper.GetDpi(ImageCanvas);
+            var width = ImageCanvas.ActualWidth / 96 * dpi.PixelsPerInchX;
+            var height = ImageCanvas.ActualHeight / 96 * dpi.PixelsPerInchY;
+            ViewModel.ImageWidth = Math.Max(1, (int)width);
+            ViewModel.ImageHeight = Math.Max(1, (int)height);
         }
     }
 }
--- Builtin.Visualizer.Wpf/DoubleVisualizerInstanceControl.xaml.cs
+++ Builtin.Visualizer.Wpf/DoubleVisualizerInstanceControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using OpenSense.Component.Builtin.Visualizer;
 
@@ -10,19 +12,39 @@ namespace OpenSense.Wpf.Component.Builtin.Visualizer {
             InitializeComponent();
         }
 
-        private void UserControl_DataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e) {
+        private void UserControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
             if (e.OldValue is DoubleVisualizer old) {
                 CompositionTarget.Rendering -= old.RenderingCallback;
             }
             if (e.NewValue is DoubleVisualizer @new) {
                 CompositionTarget.Rendering += @new.RenderingCallback;
             }
+            UpdateImageSize();
         }
 
-        private void UserControl_Unloaded(object sender, System.Windows.RoutedEventArgs e) {
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e) {
             if (ViewModel != null) {
                 CompositionTarget.Rendering -= ViewModel.RenderingCallback;
             }
+        }
+
+        private void ImageCanvas_Changed(object sender, RoutedEventArgs e) {
+            UpdateImageSize();
+        }
+
+        private void UpdateImageSize() {
+            if (ImageCanvas.ActualWidth == 0 || ImageCanvas.ActualHeight == 0) {
+                //To bypass false invokes, this kind of invokation happens every time after the image width and height are set.
+                return;
+            }
+            if (ViewModel is null) {
+                return;
+            }
+            var dpi = VisualTreeHelper.GetDpi(ImageCanvas);
+            var width = ImageCanvas.ActualWidth / 96 * dpi.PixelsPerInchX;
+            var height = ImageCanvas.ActualHeight / 96 * dpi.PixelsPerInchY;
+            ViewModel.ImageWidth = Math.Max(1, (int)width);
+            ViewModel.ImageHeight = Math.Max(1, (int)height);
         }
     }
 }
