c7abe5876b8fed35b95d46e1faeb59f1ac4d1174
Kana <orzfirst@vip.qq.com>
2023-03-18 14:31:41 -0700

    Add AU component project template

---
+++ Components/PortableFACS/ActionUnitDetector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Psi;
+using Microsoft.Psi.Components;
+using Microsoft.Psi.Imaging;
+
+namespace OpenSense.Components.PortableFACS {
+    public sealed class ActionUnitDetector : IConsumerProducer<Shared<Image>, IReadOnlyDictionary<int, float>> {
+        public Receiver<Shared<Image>> In { get; }
+
+        public Emitter<IReadOnlyDictionary<int, float>> Out { get; }
+
+        public ActionUnitDetector(Pipeline pipeline) {
+            In = pipeline.CreateReceiver<Shared<Image>>(this, Process, nameof(In));
+            Out = pipeline.CreateEmitter<IReadOnlyDictionary<int, float>>(this, nameof(Out));
+        }
+
+        private void Process(Shared<Image> image, Envelope envelope) {
+            if (image is null) {
+                return;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Components/PortableFACS/FaceImageAligner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Mediapipe.Net.Framework.Protobuf;
+using Microsoft.Psi;
+using Microsoft.Psi.Imaging;
+
+namespace OpenSense.Components.PortableFACS {
+    public sealed class FaceImageAligner : 
+        IConsumer<(Shared<Image>, IReadOnlyList<NormalizedLandmarkList>)>, 
+        IProducer<IReadOnlyList<Shared<Image>>> 
+        {
+
+        public Receiver<(Shared<Image>, IReadOnlyList<NormalizedLandmarkList>)> In { get; }
+
+        public Emitter<IReadOnlyList<Shared<Image>>> Out { get; }
+
+        public FaceImageAligner(Pipeline pipeline) {
+            In = pipeline.CreateReceiver<(Shared<Image>, IReadOnlyList<NormalizedLandmarkList>)>(this, Process, nameof(In));
+            Out = pipeline.CreateEmitter<IReadOnlyList<Shared<Image>>>(this, nameof(Out));
+        }
+
+        private void Process((Shared<Image>, IReadOnlyList<NormalizedLandmarkList>) data, Envelope envelope) {
+            var (image, lmList) = data;
+            foreach (var lm in lmList) {
+
+            }
+        }
+    }
+}
