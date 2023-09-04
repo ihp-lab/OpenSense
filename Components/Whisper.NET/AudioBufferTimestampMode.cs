bea8499687866ef38cab387b52f126501ecbc053
Zongjian Li <orzfirst@vip.qq.com>
2023-09-03 18:53:03 -0700

    A rough integration of OpenAI Whisper.

---
+++ Components/Whisper.NET/AudioBufferTimestampMode.cs
@@ -0,0 +1,7 @@
+﻿namespace OpenSense.Components.Whisper.NET {
+    public enum AudioBufferTimestampMode {
+        NotSet = 0,
+        AtStart = 2,
+        AtEnd = 1,
+    }
+}
---
+++ Components/Whisper.NET/WhisperConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Psi;
+using OpenSense.Components.Contract;
+
+namespace OpenSense.Components.Whisper.NET {
+    [Serializable]
+    internal class WhisperConfiguration : ConventionalComponentConfiguration {
+
+        public override IComponentMetadata GetMetadata() => new WhisperMetadata();
+
+        protected override object Instantiate(Pipeline pipeline, IServiceProvider serviceProvider) => new WhisperProcessor(pipeline) {
+            Logger = (serviceProvider?.GetService(typeof(ILoggerFactory)) as ILoggerFactory)?.CreateLogger(Name),
+        };
+    }
+}
