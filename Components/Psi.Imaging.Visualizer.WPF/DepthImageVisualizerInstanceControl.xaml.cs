10dbdbecf8fc85b7e6257a5e31d3e78c0fefe47b
Zongjian Li <orzfirst@vip.qq.com>
2023-10-05 09:34:13 -0700

    Fix visualization bugs

--- Components/Psi.Imaging.Visualizer.WPF/DepthImageVisualizerInstanceControl.xaml.cs
+++ Components/Psi.Imaging.Visualizer.WPF/DepthImageVisualizerInstanceControl.xaml.cs
@@ -1,9 +1,30 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using OpenSense.Components.Psi.Imaging.Visualizer;
 
 namespace OpenSense.WPF.Components.Psi.Imaging.Visualizer {
-    public partial class DepthImageVisualizerInstanceControl : UserControl {
+    public sealed partial class DepthImageVisualizerInstanceControl : UserControl {
+        
+        private DepthImageVisualizer ViewModel => (DepthImageVisualizer)DataContext;
+
         public DepthImageVisualizerInstanceControl() {
             InitializeComponent();
+        }
+
+        private void UserControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
+            if (e.OldValue is DepthImageVisualizer old) {
+                CompositionTarget.Rendering -= old.RenderingCallback;
+            }
+            if (e.NewValue is DepthImageVisualizer @new) {
+                CompositionTarget.Rendering += @new.RenderingCallback;//will be called before every time WPF wants to render a frame
+            }
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e) {
+            if (ViewModel != null) {
+                CompositionTarget.Rendering -= ViewModel.RenderingCallback;
+            }
         }
     }
 }
