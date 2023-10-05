c4c052d068fbf0b0864371d5efaeedc7623bc3d5
Zongjian Li <orzfirst@vip.qq.com>
2023-10-05 08:58:26 -0700

    Move widget libraries to the Widgets folder to make the root folder have fewer folders

---
+++ Widgets/OpenSmileConfigurationConverter.WPF/WidgetMetadata.cs
@@ -0,0 +1,13 @@
+﻿using System.Composition;
+using System.Windows;
+
+namespace OpenSense.WPF.Widgets.OpenSmileConfigurationConverter {
+    [Export(typeof(IWidgetMetadata))]
+    public class WidgetMetadata : IWidgetMetadata {
+        public string Name => "OpenSmile Configuration Converter";
+
+        public string Description => "Modify selected standard openSMILE configuration file taking usage of OpenSense.Components.OpenSmile built-in wave source and data sink components.";
+
+        public Window Create() => new ConverterWindow();
+    }
+}
