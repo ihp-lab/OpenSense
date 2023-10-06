a9e6df20197a2f41f54391c11f957affedacc125
Kana <orzfirst@vip.qq.com>
2023-10-05 19:13:47 -0700

    Fusion operator input order bug fix

--- Components/Psi.Builtin/JoinOperatorConfiguration.cs
+++ Components/Psi.Builtin/JoinOperatorConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Microsoft.Psi;
 
 namespace OpenSense.Components.Psi {
@@ -11,8 +12,16 @@ namespace OpenSense.Components.Psi {
         public override IComponentMetadata GetMetadata() => new JoinOperatorMetadata();
 
         public override object Instantiate(Pipeline pipeline, IReadOnlyList<ComponentEnvironment> instantiatedComponents, IServiceProvider serviceProvider) {
-            var producers = this.GetRemoteProducers(instantiatedComponents);
-            Debug.Assert(producers.Count == 2);
+            var metadata = GetMetadata();
+            var inputs = metadata
+                .InputPorts()
+                .Cast<FusionPortMetadata>()
+                .ToArray();
+            var producers = this.GetRemoteProducerMappings(instantiatedComponents)
+                .OrderBy(m => inputs.Single(i => Equals(i.Name, m.InputConfiguration.LocalPort.Identifier)).Order)//We need to order the inputs, because it may be out of order.
+                .Select(m => m.Producer)
+                .ToArray();
+            Debug.Assert(producers.Length == 2);
             var producer = Operators.Join(producers[0], producers[1], Inputs[0].DeliveryPolicy, Inputs[1].DeliveryPolicy);
             return producer;
         }
