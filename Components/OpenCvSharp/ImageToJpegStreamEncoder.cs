a3521ccfc94e0879506bd50ddcb2bd83ccf91bcc
Zongjian Li <orzfirst@vip.qq.com>
2023-11-10 12:57:24 -0800

    Support different pixel formats

--- Components/OpenCvSharp/ImageToJpegStreamEncoder.cs
+++ Components/OpenCvSharp/ImageToJpegStreamEncoder.cs
@@ -1,22 +1,52 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.Psi.Imaging;
 using OpenCvSharp;
 
 namespace OpenSense.Components.OpenCvSharp {
     public sealed class ImageToJpegStreamEncoder : IImageToStreamEncoder {
 
-        /// <inheritdoc/>
-        public string Description => $"Jpeg({Quality})";
-
         /// <summary>
         /// Gets or sets JPEG image quality (0-100).
         /// </summary>
         public int Quality { get; set; } = 100;
 
+
+        #region IImageToStreamEncoder
+        /// <inheritdoc/>
+        public string Description => $"Jpeg({Quality})";
+
+        /// <inheritdoc/>
         public void EncodeToStream(Image image, Stream stream) {
-            using var mat = image.ToMat();
-            var param = new ImageEncodingParam(ImwriteFlags.JpegQuality, Quality);
-            mat.WriteToStream(stream, ".jpg", param);
-        }
+            Mat mat;
+            switch (image.PixelFormat) {
+                case PixelFormat.BGR_24bpp:
+                case PixelFormat.Gray_8bpp:
+                case PixelFormat.Gray_16bpp://Not sure how this is processed
+                    mat = image.ToMat();
+                    break;
+                case PixelFormat.RGB_24bpp: {
+                        using var temp = image.ToMat();
+                        mat = new Mat(image.Height, image.Width, MatType.CV_8UC3);
+                        Cv2.CvtColor(temp, mat, ColorConversionCodes.RGB2BGR);
+                    }
+                    break;
+                case PixelFormat.BGRA_32bpp: {
+                        using var temp = image.ToMat();
+                        mat = new Mat(image.Height, image.Width, MatType.CV_8UC3);
+                        Cv2.CvtColor(temp, mat, ColorConversionCodes.BGRA2BGR);
+                    }
+                    break;
+                default:
+                    throw new NotSupportedException($"Pixel format {image.PixelFormat} is not supported.");
+            }
+            try {
+                var param = new ImageEncodingParam(ImwriteFlags.JpegQuality, Quality);
+                mat.WriteToStream(stream, ".jpg", param);
+            } finally {
+                mat.Dispose();
+            }
+        } 
+        #endregion
     }
 }
--- Components/OpenCvSharp/SharedImageExtensions.cs
+++ Components/OpenCvSharp/SharedImageExtensions.cs
@@ -4,10 +4,13 @@ using Microsoft.Psi.Imaging;
 namespace OpenSense.Components.OpenCvSharp {
     public static class SharedImageExtensions {
 
+        /// <summary>
+        /// Encodes an image to a JPEG stream.
+        /// </summary>
         public static IProducer<Shared<EncodedImage>> EncodeJpeg(
             this IProducer<Shared<Image>> image, 
             int quality = 90,
-            DeliveryPolicy<Shared<Image>> deliveryPolicy = null,
+            DeliveryPolicy<Shared<Image>>? deliveryPolicy = null,
             string name = "Encode"
             ) {
             var encoder = new ImageToJpegStreamEncoder() { 
