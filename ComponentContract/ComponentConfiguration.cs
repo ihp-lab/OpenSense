aa550a8aae6a22b7b494f942100d23c3be9fbb1b
Zongjian Li <orzfirst@vip.qq.com>
2024-05-25 18:05:15 -0700

    Fix nullable

--- ComponentContract/ComponentConfiguration.cs
+++ ComponentContract/ComponentConfiguration.cs
@@ -16,7 +16,7 @@ namespace OpenSense.Components {
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null) {
+        protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null) {
             if (!EqualityComparer<T>.Default.Equals(field, value)) {
                 field = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
